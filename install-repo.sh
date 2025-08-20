cb0f7cf3771040b92f72f54420ea9ebee0af43f9
Christian Gebauer <mail@christiangebauer.de>
2025-08-20 15:19:13 +0200

    fixed architecture

--- install-repo.sh
+++ install-repo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 curl -fsSL https://gtirepo.s3.eu-central-1.amazonaws.com/gtirepo.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/gtirepo.gpg
-echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/gtirepo.gpg] https://gtirepo.s3.eu-central-1.amazonaws.com bookworm main" | sudo tee /etc/apt/sources.list.d/gtirepo.list
+echo "deb [arch=arm64 signed-by=/etc/apt/trusted.gpg.d/gtirepo.gpg] https://gtirepo.s3.eu-central-1.amazonaws.com bookworm main" | sudo tee /etc/apt/sources.list.d/gtirepo.list
 sudo apt update
